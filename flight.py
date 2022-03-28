337f4874f1ae7fa2fd68f3a9da1a79f7e0f45aaf
ScottaBout <scott@boutfamily.com>
2022-03-28 14:28:00 -0500

    Include logging of kalman quaternion data versus optitrack quaternion data

--- flight.py
+++ flight.py
@@ -137,18 +137,18 @@ class SimpleClient:
         for v in logconf.variables:
             self.data[v.name]['time'].append(timestamp)
             self.data[v.name]['data'].append(data[v.name])
-            # if v.name == 'kalman.q0':
-            #     internal_kalman[0] = data[v.name]
-            # elif v.name == 'kalman.q1':
-            #     internal_kalman[1] = data[v.name]
-            # elif v.name == 'kalman.q2':
-            #     internal_kalman[2] = data[v.name]
-            # elif v.name == 'kalman.q3':
-            #     internal_kalman[3] = data[v.name]
+            if v.name == 'kalman.q0':
+                internal_kalman[0] = data[v.name]
+            elif v.name == 'kalman.q1':
+                internal_kalman[1] = data[v.name]
+            elif v.name == 'kalman.q2':
+                internal_kalman[2] = data[v.name]
+            elif v.name == 'kalman.q3':
+                internal_kalman[3] = data[v.name]
 
-            # if internal_kalman[0] != 0.0 and internal_kalman[1] != 0.0 and internal_kalman[2] != 0.0 and internal_kalman[3] != 0.0:
-            #     print(f'INTERNAL qw = {internal_kalman[0]}, qx = {internal_kalman[1]}, qy = {internal_kalman[2]}, qz = {internal_kalman[3]}')
-            #     internal_kalman = np.zeros(4)
+            if internal_kalman[0] != 0.0 and internal_kalman[1] != 0.0 and internal_kalman[2] != 0.0 and internal_kalman[3] != 0.0:
+                print(f'INTERNAL qw = {internal_kalman[0]}, qx = {internal_kalman[1]}, qy = {internal_kalman[2]}, qz = {internal_kalman[3]}')
+                internal_kalman = np.zeros(4)
 
     def log_error(self, logconf, msg):
         print(f'Error when logging {logconf}: {msg}')
@@ -248,9 +248,6 @@ def optitrack(queue: Queue, run_process: Value):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind(('0.0.0.0', int(CLIENT_PORT)))
         print(f'Starting socket listener')
-        if run_process.value == 0:
-            print('figure out why value is 0')
-
         while run_process.value == 1:
             data = s.recvfrom(1024)[0]
             if not data:

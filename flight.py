68d3f9f9536516039da0faf165e34d5cb285f7de
ScottaBout <scott@boutfamily.com>
2022-03-28 15:27:29 -0500

    Square test

--- flight.py
+++ flight.py
@@ -343,12 +343,12 @@ if __name__ == '__main__':
     # client.move(0.0, 0.0, 0.50, 0.0, 5.0)
 
     # Fly in a square five times (with a pause at each corner)
-    # num_squares = 2
-    # for i in range(num_squares):
-    #     client.move(0.5, 0.0, 0.5, 0.0, 2.0)
-    #     client.move(0.5, 0.5, 0.5, 0.0, 2.0)
-    #     client.move(0.0, 0.5, 0.5, 0.0, 2.0)
-    #     client.move(0.0, 0.0, 0.5, 0.0, 2.0)
+    num_squares = 2
+    for i in range(num_squares):
+        client.move(0.5, 0.0, 0.5, 0.0, 2.0)
+        client.move(0.5, 0.5, 0.5, 0.0, 2.0)
+        client.move(0.0, 0.5, 0.5, 0.0, 2.0)
+        client.move(0.0, 0.0, 0.5, 0.0, 2.0)
 
     # Go back to hover (with zero yaw) and prepare to land
     client.move(0.0, 0.0, 0.50, 0.0, 1.0)
